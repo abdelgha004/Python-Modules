24d1988b4ad3e6d383761240248024aaf357fad8
aakourya <abdelghafourakouryah@gmail.com>
2026-02-03 18:19:18 +0100

    ex0 ex1

---
+++ Python_Module_03/ex0/ft_command_quest.py
@@ -0,0 +1,16 @@
+import sys
+
+print("=== Command Quest ===")
+count = len(sys.argv)
+if count == 1:
+    print("No arguments provided!")
+    print(f"Program name: {sys.argv[0]}")
+    print(f"Total arguments: {count}")
+else:
+    print(f"Program name: {sys.argv[0]}")
+    print(f"Arguments received: {count - 1}")
+
+    for i in range(1, count):
+        print(f"Argument {i}: {sys.argv[i]}")
+
+    print(f"Total arguments: {count}")
---
+++ Python_Module_03/ex1/ft_score_analytics.py
@@ -0,0 +1,22 @@
+import sys
+
+size = len(sys.argv)
+if size > 1:
+    numbers = []
+    print("=== Player Score Analytics ===")
+    for i in range(1, size):
+        try:
+            numbers.append(int(sys.argv[i]))
+        except ValueError:
+            print(f"oops, I typed '{sys.argv[i]}' instead of '1000'")
+    size = len(numbers)
+    if(size > 0):
+        print(f"Scores processed: {numbers}")
+        print(f"Total players: {size}")
+        print(f"Total score: {sum(numbers)}")
+        print(f"Average score: {sum(numbers) / (size)}")
+        print(f"High score: {max(numbers)}")
+        print(f"Low score: {min(numbers)}")
+        print(f"Score range: {max(numbers) - min(numbers)}")
+else:
+    print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
