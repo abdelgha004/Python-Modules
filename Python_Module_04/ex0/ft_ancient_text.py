505348afc6b78cc107d3afcd0d35bce629beff71
aakourya <abdelghafourakouryah@gmail.com>
2026-03-14 12:50:18 +0000

    finish

--- Python_Module_04/ex0/ft_ancient_text.py
+++ Python_Module_04/ex0/ft_ancient_text.py
@@ -1,12 +1,12 @@
-f = open("ancient_fragment.txt", "r")
+try:
+    f = open("ancient_fragment.txt", "r")
+    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
+    print("Accessing Storage Vault: ancient_fragment.txt")
+    print("Connection established...\n")
+    print("RECOVERED DATA:")
+    print(f.read())
+    f.close()
+    print("\nData recovery complete. Storage unit disconnected.")
 
-print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n")
-print("Accessing Storage Vault: ancient_fragment.txt")
-print("Connection established...")
-print("\nRECOVERED DATA:")
-
-print(f.read())
-
-f.close()
-
-print("\nData recovery complete. Storage unit disconnected.")
+except FileNotFoundError:
+    print("ERROR: Storage vault not found. Run data generator first.")
